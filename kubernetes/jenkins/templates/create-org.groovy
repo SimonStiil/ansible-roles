5944e9c56b357b16433f212a6d874fe58d33348b
simon <130985998+SimonStiil@users.noreply.github.com>
2024-02-15 18:18:17 +0100

    adding comments and switching to pip

--- kubernetes/jenkins/templates/create-org.groovy
+++ kubernetes/jenkins/templates/create-org.groovy
@@ -5,6 +5,7 @@ navigator.traits = [
   new org.jenkinsci.plugins.github_branch_source.TopicsTrait('jenkins-kubernetes'),
   new org.jenkinsci.plugins.github_branch_source.BranchDiscoveryTrait(1), // Exclude branches that are also filed as PRs.
   new org.jenkinsci.plugins.github_branch_source.OriginPullRequestDiscoveryTrait(1),
+  new org.jenkinsci.plugins.github_branch_source.TagDiscoveryTrait(),
 ]
 navigator.credentialsId = "github-login-secret"
 folder.navigators.replace(navigator)
@@ -19,4 +20,11 @@ folder.addTrigger(new com.cloudbees.hudson.plugins.folder.computed.PeriodicFolde
 folder.setOrphanedItemStrategy(new com.cloudbees.hudson.plugins.folder.computed.DefaultOrphanedItemStrategy(true, 7, -1))
 folder.save()
 
+//For adding functionality:
+//def folder = Jenkins.instance.getItem("github")
 //Jenkins.instance.items[0].buildStrategies.owner
+
+// examples:
+// https://javadoc.jenkins.io/plugin/branch-api/jenkins/branch/OrganizationFolder.html
+// https://javadoc.jenkins.io/plugin/github-branch-source/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.html
+// https://javadoc.jenkins.io/plugin/github-branch-source/org/jenkinsci/plugins/github_branch_source/TagDiscoveryTrait.html
